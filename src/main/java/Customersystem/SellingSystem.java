c8dcae02c961f831e3a4a919ef7c51ded6331d9b
Jhony Lopez <jhonylopezareiza@gmail.com>
2026-02-10 22:34:48 -0500

    feat: US-003 crud product

--- src/main/java/Customersystem/SellingSystem.java
+++ src/main/java/Customersystem/SellingSystem.java
@@ -15,11 +15,16 @@ public class SellingSystem {
 
     //Customers Atributes
 
+    static int idCustomer;
     static String customerName;
-    static int customerPhone;
+    static String customerPhone;
+    static String customerEmail;
+
 
     public static void main(String[] args) {
+
    createProducts();
+
    getProducts();
      System.out.println("Ingrese el id del producto a actualizar: ");
    int id = sc.nextInt();
@@ -27,9 +32,21 @@ public class SellingSystem {
    updateProducts(id);
      System.out.println("Ingrese el id del producto a eliminar: ");
       id = sc.nextInt();
+
   deleteProduct(id);
+
   createCustomer();
 
+  getCustomers();
+        System.out.println("Ingrese el Id del cliente a actualizar.");
+        int findUpdateId = sc.nextInt();
+        sc.nextLine();
+
+        updateCustomers(findUpdateId);
+        System.out.println("Ingrese el Id del cliente a eliminar.");
+        int findDeleteId = sc.nextInt();
+
+        deleteCustomer(findDeleteId);
 
     }
 
@@ -81,7 +98,6 @@ public class SellingSystem {
       System.out.println("Producto no encontrado.");
      }
 
-
  }
 
  public static void deleteProduct( int deleteId){
@@ -92,20 +108,64 @@ public class SellingSystem {
         quantity = 0;
         state = false;
      }else {
-      System.out.println("Id " + id + "no encontrado.");
+      System.out.println("Id " + deleteId + " no encontrado.");
      }
  }
 
+ //Métodos clientes
+
  public static void createCustomer(){
+
+     System.out.println("Ingrese el ID del cliente: ");
+     idCustomer = sc.nextInt();
+     sc.nextLine();
      System.out.println("Ingrese el nombre del cliente: ");
      customerName = sc.nextLine();
-     sc.nextLine();
-
      System.out.println("Ingrese el teléfono del cliente");
-     customerPhone = sc.nextInt();
-     sc.nextLine();
+     customerPhone = sc.nextLine();
+     System.out.println("Ingrese el correo del cliente: ");
+     customerEmail = sc.nextLine();
+     System.out.println("Estado del cliente (true = activo, false = inactivo)");
+     state = sc.nextBoolean();
 
  }
 
+public static void getCustomers(){
+    System.out.println("Id: " + idCustomer + "\n" +
+            "Nombre del cliente: " + customerName + "\n" +
+            "Teléfono: " + customerPhone + "\n" +
+            "Correo: " + customerEmail + "\n" +
+            "Estado: " + state + "\n");
+}
+
+    public static void updateCustomers(int findId) {
+        if (findId == idCustomer) {
+            System.out.println("Ingrese el ID del cliente: ");
+            idCustomer = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Ingrese el nombre del cliente: ");
+            customerName = sc.nextLine();
+            System.out.println("Ingrese el teléfono del cliente");
+            customerPhone = sc.nextLine();
+            System.out.println("Ingrese el correo del cliente: ");
+            customerEmail = sc.nextLine();
+            System.out.println("Estado del cliente (true = activo, false = inactivo)");
+            state = sc.nextBoolean();
+        } else {
+            System.out.println("Cliente no encontrado.");
+        }
+    }
+
+    public static void deleteCustomer( int deleteId) {
+        if (deleteId == idCustomer){
+            idCustomer = 0;
+            customerName = null;
+            customerPhone =null;
+            customerEmail = null;
+            state = false;
+        }else {
+            System.out.println("Id " + deleteId + " No encontrado.");
+        }
+    }
 
 }
